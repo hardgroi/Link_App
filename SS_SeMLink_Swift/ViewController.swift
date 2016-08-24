e7ee1e54ae05d2aa1a7ba7160b6bfd29bd359a9a
Ian Hardgrove <imhardgrove@gmail.com>
2016-08-24 14:53:55 -0400

    Initial Commit

---
+++ SS_SeMLink_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SS_SeMLink_Swift
+//
+//  Created by Ian Hardgrove on 8/24/16.
+//  Copyright © 2016 Ian Hardgrove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
